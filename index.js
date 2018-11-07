afbffa481389e03327ce1c9047c3881db35660ce
CGua <cgua@cguacore.com>
2018-11-07 16:51:07 +0800

    com

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 const { main } = require('./lib/main');
 
 main('./md', './_pub');
+
+
+// new Promise((res, rej) => {
+//     res('res');
+// })
+//     .then((args) => {
+//         console.log(args);
+//     })
+//     .then((args) => {
+//         console.log(args);
+
+//         return '???';
+//     })
+//     .then((args) => new Promise((res, rej) => {
+//         console.log(args);
+//         setTimeout(() => {
+//             res('p2');
+//         }, 1000);
+//     }))
+//     .then((args) => {
+//         console.log(args);
+//     });
